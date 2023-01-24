1757c8865c401c4714eb201dd0e17c0e2da8977d
Paolo <paolo@priam.ai>
2023-01-24 07:05:42 +0000

    Added validators.

--- feeds/phishing-database/tests/phishing_test.py
+++ feeds/phishing-database/tests/phishing_test.py
@@ -1,7 +1,8 @@
 import unittest
 import requests
 import ipaddress
-
+import validators
+from stix2 import Bundle, Identity, Indicator, IPv4Address,IPv6Address, Relationship, DomainName, URL
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -11,6 +12,12 @@ logger.setLevel(logging.INFO)
 class DownloadTestCase(unittest.TestCase):
     ROOT_URL = 'https://raw.githubusercontent.com/mitchellkrogza/Phishing.Database/master'
     IP_PHISH_ACTIVE = 'phishing-IPs-ACTIVE.txt'
+
+    def setUp(self) -> None:
+        self.author = Identity(
+            name="Phishing Database",
+            identity_class="organization",
+        )
     def test_IP_phish(self):
         for status in ['ACTIVE','INACTIVE','INVALID']:
             logger.info(f'IP {status}')
@@ -20,12 +27,29 @@ class DownloadTestCase(unittest.TestCase):
                 total = 0
                 for line in r.content.decode().split('\n'):
                     ipstr = line.strip()
+
                     if len(ipstr)>0:
                         try:
-                            ip = ipaddress.ip_address(ipstr)
-
-                            if ip.version == 4: pass
-                            elif ip.version == 6: pass
+                            if validators.ip_address.ipv4(ipstr):
+                                observable = IPv4Address(value=ipstr)
+                                indicator = Indicator(
+                                    name=observable.value,
+                                    description="Phishing IP",
+                                    created_by_ref=f"{self.author.id}",
+                                    pattern_type="stix",
+                                    pattern=f"[ipv4:value = '{observable.value}']",
+                                    labels="phishing",
+                                )
+                            elif validators.ip_address.ipv6(ipstr):
+                                observable = IPv6Address(value=ipstr)
+                                indicator = Indicator(
+                                    name=observable.value,
+                                    description="Phishing IP",
+                                    created_by_ref=f"{self.author.id}",
+                                    pattern_type="stix",
+                                    pattern=f"[ipv6:value = '{observable.value}']",
+                                    labels="phishing",
+                                )
                             total+=1
                         except Exception as e:
                             logger.error(f"Invalid IP address {ipstr}")
@@ -42,8 +66,9 @@ class DownloadTestCase(unittest.TestCase):
                 total = 0
                 for line in r.content.decode().split('\n'):
                     domain = line.strip()
-                    if len(domain)>0:
+                    if len(domain)>0 and validators.domain(domain):
                         total+=1
+                        observable = DomainName(value=domain)
 
                 self.assertGreater(total,0)
             else:
@@ -58,8 +83,9 @@ class DownloadTestCase(unittest.TestCase):
                 total = 0
                 for line in r.content.decode().split('\n'):
                     url = line.strip()
-                    if len(url)>0:
+                    if len(url)>0 and validators.url(url):
                         total+=1
+                        observable = URL(value=url)
                 self.assertGreater(total,0)
             else:
                 raise Exception('Feed URL is down')
